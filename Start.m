5efb86344dc7fc5b077c8dbab050a328c36ca91e
Francesco <francesco.argentieri89@gmail.com>
2018-05-07 09:41:10 +0200

    answer point 3

--- Start.m
+++ Start.m
@@ -1,7 +1,7 @@
 %% Automatic control project
-close all;
-clear;
-clc;
+close all
+clear
+clc
 addpath(genpath('/Users/francesco/Documents/MATLAB/Automatic Control/toolboxes'));
 yalmip('clear');
 
@@ -37,9 +37,10 @@ u = [1 0].';
 
 A = [zeros(2) eye(2); -M\K -M\Cdamp];   % left divide for the inverse
 B = [zeros(2,1); M\u];                  % single input
-C = [1 0 0 0];                          % multiple output
+C = [1 0 0 0];                          % single output
+D = 0;
+sys = ss(A,B,C,D);
 
-sys = ss(A,B,C,[]);
 %% Convert State-Space Model to Transfer Function
 tf(sys)
 
@@ -61,7 +62,7 @@ yalmip('clear')
 
 % set the parameters of the LMI solver
 opts = sdpsettings;     % set the solver settings
-opts.solver = 'Lmilab'; % chose the solver
+opts.solver = 'sdpt3';  % chose the solver
 opts.verbose = 1;       % show iterations 0 = don't show, 1 = show
 
 % set the variable to optimize
@@ -70,15 +71,66 @@ obj = trace(P);                             % set the objective function
 constr = [ A'* P + P * A < 0; P > eye(length(A))];  % set the constraints
 
 % solve the problem
-yalmipdiagnostics = solvesdp(constr,obj,opts);
+yalmipdiagnostics = optimize(constr,obj,opts);
 
 % display the results during iteration
-msg = ['feasibility = ', num2str(yalmipdiagnostics.problem)];
-disp(msg);
+if yalmipdiagnostics.problem == 0
+    disp('Solver thinks it is feasible')
+elseif yalmipdiagnostics.problem == 1
+    disp('Solver thinks it is infeasible')
+else
+    disp('Something else happened')
+end
+
 P = double(P); % extract the result
 disp(P);
 
 % check that is a proper Lyapunov function
-[V,D,W] = eig(A' * P + P * A);
+[V,E] = eig(A' * P + P * A);
 disp(V); % V of right eigenvectors
-disp(D); % D of eigenvalues
+disp(E); % E of eigenvalues
+
+% check the constraint
+check(constr);
+save statespace.mat A B C D
+
+%% 3) estimate numerically L2 gain
+clear
+load statespace.mat;
+yalmip('clear')
+% decision variable
+P = sdpvar(length(A));
+gamma = sdpvar(1);      % scalar
+
+n = length(A);
+[ny, nu] = size(D);
+
+% define inequality constraint
+M = [P * A          P * B           zeros(n,ny);
+    zeros(nu,n) -gamma/2*eye(nu)    zeros(nu,ny);
+    C                 D             -gamma/2*eye(ny)'];
+
+constr = [M + M' < 0, P > 0];
+
+opts = sdpsettings;
+opts.solver = 'sdpt3';
+
+diagnostic = optimize(constr,gamma,opts);
+if diagnostic.problem == 0
+    disp('Solver thinks it is feasible')
+elseif diagnostic.problem == 1
+    disp('Solver thinks it is infeasible')
+else
+    disp('Something else happened')
+end
+
+% evaluate solution variables (if any)
+Psol=double(P);
+gammasol=double(gamma);
+disp(Psol);
+disp(gammasol);
+
+% compare to alternative Hinf norm computation
+sys = pck(A,B,C,D);
+out = hinfnorm(sys);
+disp([out(2) gammasol])
